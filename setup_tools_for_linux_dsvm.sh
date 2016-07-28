08806a262e11dc3475bc86e0b91d49063e0b9c62
hoaphumanoid <miguelgfierro@gmail.com>
2016-07-28 10:42:09 +0100

    added license and initial script

---
+++ setup_tools_for_linux_dsvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# This script installs several libraries for developing deep learning applications
+#
+# Script specifications, change
+INSTALL_FOLDER=installer
+KERAS_VERSION=1.0.6
+
+# Create installation folder
+mkdir ~/$INSTALL_FOLDER
+
+# Install keras
+echo "Installing keras library version $KERAS_VERSION"
+pip install keras=$KERAS_VERSION
+
+
