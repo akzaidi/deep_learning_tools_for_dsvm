48128adccaba99d0cf0a9d7e2229b92fdfb006c1
hoaphumanoid <miguelgfierro@gmail.com>
2016-07-28 14:56:10 +0100

    torch

--- setup_tools_for_linux_dsvm.sh
+++ setup_tools_for_linux_dsvm.sh
@@ -5,13 +5,28 @@
 # Script specifications, change
 THIS_FOLDER=$PWD
 INSTALL_FOLDER=installer
+OPENBLAS_VERSION=0.2.18
 THEANO_VERSION=0.8.2
 KERAS_VERSION=1.0.6
 SKLEARN_VERSION=0.17.1
-OPENBLAS_VERSION=0.2.18
+CHAINER_VERSION=1.12.0
+TORCH_VERSION=2
 
 # Create installation folder
 mkdir ~/$INSTALL_FOLDER
+
+# Install openblas
+# FIXME: check if the version is the same, if nto, then update
+echo "Installing open-blas version $OPENBLAS_VERSION"
+OPENBLAS_FILE=/usr/local/include/openblas_config.h
+if [ ! -e $OPENBLAS_FILE ] ; then
+	cd ~/$INSTALL_FOLDER
+	git clone --branch v$OPENBLAS_VERSION https://github.com/xianyi/OpenBLAS/
+	cd OpenBLAS
+	make FC=gfortran -j $(($(nproc) + 1))
+	sudo make PREFIX=/usr/local install
+	cd $THIS_FOLDER
+fi
 
 #Install theano
 echo "Installing theano library version $THEANO_VERSION"
@@ -25,15 +40,22 @@ sudo `which pip` install keras==$KERAS_VERSION
 echo "Installing scikit-learn library version $SKLEARN_VERSION"
 sudo `which pip` install scikit-learn==$SKLEARN_VERSION
 
-# Install openblas
-echo "Installing open-blas version $OPENBLAS_VERSION"
+# Install chainer
+echo "Installing chainer library version $CHAINER_VERSION"
+sudo `which pip` install chainer==$CHAINER_VERSION
+
+# Install torch
+echo "Installing torch library version $TORCH_VERSION"
+sudo yum install -y readline-devel ncurses-devel libjpeg-turbo-devel libpng-devel GraphicsMagick-devel fftw-devel sox-devel sox qt-devel qtwebkit-devel 
+TORCH_FILE=/usr
 cd ~/$INSTALL_FOLDER
-git clone --branch v$OPENBLAS_VERSION https://github.com/xianyi/OpenBLAS/
-cd OpenBLAS
-make FC=gfortran -j $(($(nproc) + 1))
-sudo make PREFIX=/usr/local install
+git clone https://github.com/torch/distro.git torch --recursive
+cd torch
 cd $THIS_FOLDER
 
+# Install mxnet
+
+# Install caffe
 
 
 
