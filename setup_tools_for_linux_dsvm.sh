e549654b3390a3edb6f3aa5c864265fa390125a9
hoaphumanoid <miguelgfierro@gmail.com>
2016-07-29 14:34:11 +0100

    caffe

--- setup_tools_for_linux_dsvm.sh
+++ setup_tools_for_linux_dsvm.sh
@@ -12,6 +12,7 @@ KERAS_VERSION=1.0.6
 SKLEARN_VERSION=0.17.1
 CHAINER_VERSION=1.12.0
 TORCH_VERSION=LUAJIT21
+CAFFE_VERSION=rc3
 
 # Create installation folder
 mkdir -p $INSTALL_FOLDER
@@ -58,9 +59,21 @@ source $HOME_USER/.bashrc
 cd $THIS_FOLDER
 
 # Install caffe
-#echo "Installing caffe library version $CAFFE_VERSION"
-#yum install -y libleveldb-dev 
-#yum install --no-install-recommends libboost-all-dev
+echo "Installing caffe library version $CAFFE_VERSION"
+sudo yum install -y protobuf-compiler leveldb-devel snappy-devel opencv-devel boost-devel hdf5-devel
+sudo yum install -y gflags-devel glog-devel lmdb-devel
+cd ~/$INSTALL_FOLDER
+git clone --branch v$CAFFE_VERSION https://github.com/BVLC/caffe.git
+cd caffe
+cp Makefile.config.example Makefile.config
+sed -i 's/BLAS := atlas/BLAS := open/' Makefile.config
+sudo `which pip` install cython scikit-image h5py leveldb networkx python-gflags pillow
+make all -j $(($(nproc) + 1))
+make pycaffe -j $(($(nproc) + 1))
+echo 'export CAFFE_ROOT=$(pwd)' >> $HOME_USER/.bashrc
+echo 'export PYTHONPATH=$CAFFE_ROOT/python:$PYTHONPATH' >> $HOME_USER/.bashrc
+source $HOME_USER/.bashrc
+cd $THIS_FOLDER
 
 # Install mxnet
 
