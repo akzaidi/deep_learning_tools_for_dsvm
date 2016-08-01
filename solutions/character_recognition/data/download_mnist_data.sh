5d2e490247a2066986b315002d5b2ec49c3a0354
hoaphumanoid <miguelgfierro@gmail.com>
2016-08-01 16:27:58 +0100

    downloader of MNIST data

---
+++ solutions/character_recognition/data/download_mnist_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Downloads the MNIST data
+
+wget https://hoaphumanoidstorage2.blob.core.windows.net/public/mnist_test.csv
+wget https://hoaphumanoidstorage2.blob.core.windows.net/public/mnist_train.csv
