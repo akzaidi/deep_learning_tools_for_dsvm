d601018744e2019d628ac9c51be25bccfd660548
hoaphumanoid <miguelgfierro@gmail.com>
2016-08-02 09:29:16 +0100

    changes in the general installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#
+# This script installs a group of deep learning libraries and tools in a DSVM
+#
 
-
+# Prepare environment
 for i in "$@"
 do
 case $i in
@@ -19,9 +22,11 @@ echo "INSTALL FOLDER  = $INSTALL_FOLDER"
 echo "USER = $SESSION_USER"
 echo "HOME PATH = $SESSION_HOME"
 
-
+# Execute script that installs many deep learning libraries
 sudo config/setup_tools_for_linux_dsvm.sh $INSTALL_FOLDER $SESSION_HOME
 
+# Install mxnet
+#Rscript -e "LIB_PATH <- paste0(Sys.getenv('LD_LIBRARY_PATH'),'/usr/local/lib'); Sys.setenv(LD_LIBRARY_PATH=LIB_PATH)"
 cd $INSTALL_FOLDER/mxnet
 R CMD INSTALL mxnet_0.7.tar.gz
 
