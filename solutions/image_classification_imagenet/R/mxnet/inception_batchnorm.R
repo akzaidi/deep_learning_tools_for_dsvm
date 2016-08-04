2aa26323a508ac8b48c8171184ab725ccf420de2
hoaphumanoid <miguelgfierro@gmail.com>
2016-08-04 10:05:48 +0100

    imagenet batch pretrained mxnet R

--- solutions/image_classification_imagenet/R/mxnet/inception_batchnorm.R
+++ solutions/image_classification_imagenet/R/mxnet/inception_batchnorm.R
@@ -1,69 +1,50 @@
 
 require(mxnet)
-
-#Initialize variables
-devices <- mx.cpu()
-mx.set.seed(0)
 mx.ctx.internal.default.value = list(device="cpu",device_id=0,device_typeid=1)
 class(mx.ctx.internal.default.value) = "MXContext"
+require(imager)
 
 
-#Gather the data
-root_path <- c("../../data/")
-train_file <- file.path(root_path,"mnist_train.csv")
-test_file <- file.path(root_path,"mnist_test.csv")
-train <- read.csv(train_file, header=TRUE)
-train <- data.matrix(train)
-test <- read.csv(test_file, header=TRUE)
-test <- data.matrix(test)
-dim(train)
-dim(test)
+#Data gathering
+root_path <- c("../../data/Inception/")
+model_path <- file.path(root_path,"Inception_BN")
+model = mx.model.load(model_path, iteration=39)
+mean_path <- file.path(root_path,"mean_224.nd")
+mean.img = as.array(mx.nd.load(mean_path)[["mean_img"]])
+#Select one image
+im <- load.image(system.file("extdata/parrots.png", package="imager"))
+#plot(im)
 
-
-#Modify data
-train.x <- train[,-1]
-train.y <- train[,1]
-train.x <- t(train.x/255)
-table(train.y)#number of examples
-length(train.y)
-
-
-#Create the NN
-data <- mx.symbol.Variable("data")
-fc1 <- mx.symbol.FullyConnected(data, name="fc1", num_hidden=128)
-act1 <- mx.symbol.Activation(fc1, name="relu1", act_type="relu")
-fc2 <- mx.symbol.FullyConnected(act1, name="fc2", num_hidden=64)
-act2 <- mx.symbol.Activation(fc2, name="relu2", act_type="relu")
-fc3 <- mx.symbol.FullyConnected(act2, name="fc3", num_hidden=10)
-softmax <- mx.symbol.SoftmaxOutput(fc3, name="sm")
-
-
-#Train the NN
-time_init <- Sys.time()
-model <- mx.model.FeedForward.create(softmax, X=train.x, y=train.y,
-                                     ctx=devices, num.round=10, array.batch.size=100,
-                                     learning.rate=0.07, momentum=0.9,  eval.metric=mx.metric.accuracy,
-                                     initializer=mx.init.uniform(0.07),
-                                     epoch.end.callback=mx.callback.log.train.metric(100))
-time_end <- Sys.time()
-difftime(time_end, time_init, units = "mins")
-
-
-# Prediction of test set
-test.x <- test[,-1]
-test.y <- test[,1]
-test.x <- t(test.x/255)
-table(test.y)#number of examples
-length(test.y)
-preds <- predict(model, test.x)
-pred.label = max.col(t(preds))-1
-dim(preds)
-length(pred.label)
-
-accuracy <- function(label, pred) {
-  ypred = max.col(t(as.array(pred)))
-  return(sum((as.array(label) + 1) == ypred) / length(label))
+#Image normalization
+preproc.image <- function(im, mean.image) {
+  # crop the image
+  shape <- dim(im)
+  short.edge <- min(shape[1:2])
+  xx <- floor((shape[1] - short.edge) / 2)
+  yy <- floor((shape[2] - short.edge) / 2)
+  croped <- crop.borders(im, xx, yy)
+  # resize to 224 x 224, needed by input of the model.
+  resized <- resize(croped, 224, 224)
+  # convert to array (x, y, channel)
+  arr <- as.array(resized) * 255
+  dim(arr) <- c(224, 224, 3)
+  # substract the mean
+  normed <- arr - mean.img
+  # Reshape to format needed by mxnet (width, height, channel, num)
+  dim(normed) <- c(224, 224, 3, 1)
+  return(normed)
 }
+normed <- preproc.image(im, mean.img)
+#plot(normed)
 
-print(paste0("Finish prediction... accuracy=", accuracy(test.y, preds)))
+#Predict using the pretrained model
+prob <- predict(model, X=normed)
+dim(prob)
+max.idx <- max.col(t(prob))
+max.idx
+
+synsets_path = file.path(root_path,"synset.txt")
+synsets <- readLines(synsets_path)
+print(paste0("Predicted Top-class: ", synsets[[max.idx]]))
+
 
