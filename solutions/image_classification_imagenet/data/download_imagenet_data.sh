86fc070e850d995471e72d6e2bd177d60ef9597a
hoaphumanoid <miguelgfierro@gmail.com>
2016-08-04 09:58:13 +0100

    fixed small path errors

--- solutions/image_classification_imagenet/data/download_imagenet_data.sh
+++ solutions/image_classification_imagenet/data/download_imagenet_data.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#Downloads the MNIST data
-
+#Downloads the ImageNet data
 wget https://hoaphumanoidstorage2.blob.core.windows.net/public/Inception.zip
 unzip Inception.zip
-cd Inception/Inception
-cp * ../../
-cd ../../
-rm -rf Inception
+rm  Inception.zip
