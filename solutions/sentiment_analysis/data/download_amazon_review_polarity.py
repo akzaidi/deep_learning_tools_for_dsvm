2c484c9cf47ca7434b88bcf246939cc84bc6c25a
Ilia Karmanov <ilkarman@microsoft.com>
2016-08-15 11:40:30 +0100

    Create Features
    Altered code to run faster using pandas and final output is a text of
    vectors ready to be imported into model

--- solutions/sentiment_analysis/data/download_amazon_review_polarity.py
+++ solutions/sentiment_analysis/data/download_amazon_review_polarity.py
@@ -1,16 +1,14 @@
 import numpy as np
-import csv
+import pandas as pd
 import requests
 from tqdm import tqdm
 import os.path
 
 AZ_ACC = "amazonsentimenik"
 AZ_CONTAINER = "textclassificationdatasets"
-ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+ =<>()[]{}"
-FEATURE_LEN = 1014 
-cdict = dict((c, i + 2) for i, c in enumerate(ALPHABET)) # first index for 'other' and blank for 'padding'
-print("We have %d tokens" % len(cdict))
-print(cdict)
+ALPHABET = list("abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+ =<>()[]{}")
+print("Alphabet %d characters: " % len(ALPHABET), ALPHABET)
+FEATURE_LEN = 1014
 
 
 def download_file(url):
@@ -26,33 +24,66 @@ def download_file(url):
         print('saved data')
 
 
-def create_features(infile, outfile):
+def create_features(infile, outprefix):
+    """
+    If we have 100 reviews to create features from, this will output a y-vector
+    that is 100 x 1 (containing 1 or 0 for positive and negative review), and
+    a 100 x 1014 (character/padding) x 69 (dimensions) x-tensor.
+
+    The x-tensor will be saved with a character (vector) per line, so it will
+    need to be parsed every 1014 characters (equals one review).
+
+    For example the first 4 lines = first 4 characters:
+    000000000000000000000000000000000000000000001000000000000000000000000
+    000000000000000000000000000000000000000001000000000000000000000000000
+    000000000000000000000000000000000000000000000000000000000001000000000
+    000000100000000000000000000000000000000000000000000000000000000000000"""
+
     # Get data from windows blob
     download_file('https://%s.blob.core.windows.net/%s/%s' % (AZ_ACC, AZ_CONTAINER, infile))
-    with open(outfile, 'w') as outy:
-        writer = csv.writer(outy, lineterminator='\n')
-        writer.writerow(['class'] + ["v%d" % (d+1) for d in range(FEATURE_LEN)])
-        #with open(infile, 'r', encoding="utf8") as iny: #python3
-        with open(infile, 'rb') as iny:
-            # use summary and review columns
-            reader = csv.DictReader(iny, fieldnames=['class','summary','review'])
-            for r in reader:
-                sample = np.ones(FEATURE_LEN+1) # initialise all as other char
-                # Put characters in reverse order
-                count = FEATURE_LEN
-                for col in ['summary','review']:
-                    for char in r[col].lower():
-                        if char in cdict:
-                            sample[count] = cdict[char]
-                        count -= 1
-                        if count == 1:
-                            break
-                sample[0] = int(r['class']) - 1 
-                writer.writerow(sample)
+    # load data into dataframe
+    df = pd.read_csv(infile,
+                     header=None,
+                     names=['sentiment', 'summary', 'text'])
 
-                        
+    # concat summary, review; trim to 1014 char; reverse; lower
+    df['rev'] = (df.summary + " " + df.text).str[:FEATURE_LEN].str[::-1].str.lower()
+    df.drop(['text', 'summary'], axis=1, inplace=True)
+    df.sentiment -= 1
+    Y_split = np.asarray(df.sentiment, dtype='int')
+
+    # character-encoding
+    voc_hash = np.identity(len(ALPHABET))
+    df_hash = pd.DataFrame(voc_hash, columns=ALPHABET)
+    X_split = np.zeros([df.shape[0], FEATURE_LEN, len(ALPHABET)], dtype='int')
+
+    print('creating character encoding')
+    # create character tensor
+    for ti, tx in enumerate(df.rev):
+        if (ti + 1) % 100000 == 0:
+            print("%d rows processed" % ti)
+        chars = list(tx)
+        for ci, ch in enumerate(chars):
+            if ch in ALPHABET:
+                X_split[ti][ci] = np.array(df_hash[ch])
+
+    # Save to disk in universal format
+    # Assume we have 5 reviews
+    # Y -> 5 x 1
+    print("Saving y list ...")
+    np.savetxt(outprefix + 'y.csv', Y_split, fmt='%i')
+    print(Y_split[:2])
+
+    # 3D tensor i.e. 5 by 1014 = 5070
+    print("Saving x tensor ...")
+    with open(outprefix + 'x.csv', 'wb') as outfile:
+        for data_slice in X_split:
+            # print(data_slice.shape) # (1014, 69)
+            np.savetxt(outfile, data_slice, fmt='%i'*len(ALPHABET))
+    print(X_split[:2])
+
 if __name__ == '__main__':
-    create_features(infile='amazon_review_polarity_test.csv', outfile='test_char_cnn.csv')
+    create_features(infile='amazon_review_polarity_test.csv', outprefix='amazon_test_')
     print("saved test set")
-    create_features(infile='amazon_review_polarity_train.csv', outfile='train_char_cnn.csv')
+    create_features(infile='amazon_review_polarity_train.csv', outprefix='amazon_train_')
     print("saved train set")
